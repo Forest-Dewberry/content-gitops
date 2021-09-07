527ae10dff3aec8a528a3b5bf20a37be7c5bccea
Forest Dewberry <forest.dewberry@gmail.com>
2021-09-06 19:15:01 -0500

    Update app.py

--- python/app.py
+++ python/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello ACG student, you're awesome!"
+    return "Hello ACG student, you're awesome!!"
 #comment
 
 if __name__ == "__main__":
